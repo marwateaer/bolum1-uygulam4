f50ce51038edfddde9f698c38b1947254252a659
marwateaer <145429513+marwateaer@users.noreply.github.com>
2023-09-19 17:15:35 +0300

    Add files via upload

---
+++ uygulama7/Form1.cs
@@ -0,0 +1,15 @@
+namespace uygulama7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+    }
+}
